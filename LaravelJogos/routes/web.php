6025469be337c8a3dfbb23516f3616ed67967798
Joao Moreno <161522359+JoaoVitor-Fatec@users.noreply.github.com>
2024-06-14 20:37:08 -0300

    Delete LaravelJogos directory

--- LaravelJogos/routes/web.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\JogoController;
-
-Route::resource('jogos', JogoController::class);
-Route::get('jogos/{id}/delete', [JogoController::class, 'delete'])->name('jogos.delete');
